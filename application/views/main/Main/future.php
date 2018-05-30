8d126b1f64186d710d130bc557fd388828bfb1a3
emmwee <emmwee96@gmail.com>
2018-05-30 14:27:54 +0800

    future

---
+++ application/views/main/Main/future.php
@@ -0,0 +1,28 @@
+<section class="color-dark-section">
+	<div class="container">
+		<h1>Future</h1>
+		<br/>
+		<h3>Virtual Reality Solution</h3>
+		<br/>
+		<p>Virtual Reality (VR) is a technology that oﬀers different game immersion and experience [11]. The technology generates
+			scenarios that simulate realistic experiences for the player that are near similar to the real world.</p>
+	</div>
+</section>
+<section class="star-section">
+	<div class="container">
+		<img class="img-fluid" src="<?= base_url() ?>images/future.png" style="margin-bottom:2.5%; width : 100%;">
+	</div>
+</section>
+<section class="color-dark-section">
+	<div class="container">
+        <p>Using virtual reality headsets with the necessary image, sound and other sense capacities, players can look, move around and interact items as if in the real world. They can also take actions using motion controllers.</p>
+        <br/>
+        <p>Since 2015, the growth of the VR industry has been fast paced. In 2016, revenue from VR hardware and software stood at US$3.7 billion. It is projected this will increase to more than US$40 billion by 2020.</p>
+        <br/>
+        <p>The mobile VR head-mounted display devices are forecast to account for about 75% of global VR device sales with more than 130 million users worldwide by 2020.</p>
+        <br/>
+        <p>GAMBOL intent to adopt VR technology in its architecture as part of the long term strategy. Part of the funds raised during an upcoming planned token sale will go into developing and transforming the game experience on the GAMBOL ecosystem from screen display to VR.</p>
+        <br/>
+        <p>The ﬁrst VR demo game will be launched in early 2019. Soon later, players can choose to enter a game through either screen or VR mode. </p>
+	</div>
+</section>
